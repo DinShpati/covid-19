4c070fdd73fc9cd5a62477b1227810870c2c76a0
Din Spataj <proendergamer03@gmail.com>
2020-04-02 20:25:50 +0300

    Updates

---
+++ precache-manifest.abfa41169081323a9b2d7e77487d9ab7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a911c3f2add811d2c67ff4728604323",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b03106952a8824fad98",
+    "url": "/static/css/main.a44fbfed.chunk.css"
+  },
+  {
+    "revision": "ebc0587669e75060fc87",
+    "url": "/static/js/2.ade90ecb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ade90ecb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b03106952a8824fad98",
+    "url": "/static/js/main.e795e5cd.chunk.js"
+  },
+  {
+    "revision": "e663df2881e39a90859c",
+    "url": "/static/js/runtime-main.225c0f6f.js"
+  }
+]);
