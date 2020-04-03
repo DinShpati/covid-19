af99bd11d8b736b6444b7a721f565cf2dea12db1
Din Spataj <proendergamer03@gmail.com>
2020-04-03 15:01:22 +0300

    Update nav.js

--- src/components/nav.js
+++ src/components/nav.js
@@ -9,7 +9,7 @@ function Nav(){
                 <Link to="/" className="nav-link"><i className="fas fa-globe-americas"></i><br/>Countries</Link>
                 <Link to="/map" className="nav-link"><i className="fas fa-map-marked-alt"></i><br/>Map</Link>
                 <Link to="/stats" className="nav-link"><i className="fas fa-chart-line"></i><br/>Statistics</Link>
-                <Link to="/dua" className="nav-link"><i className="fas fa-mosque"></i><br/>Dua.</Link>
+                <Link to="/dua" className="nav-link"><i className="fas fa-mosque"></i><br/>Dua</Link>
                 <Link to="/about" className="nav-link"><i className="far fa-user"></i><br/>About</Link>
             </ul>
         </nav>
